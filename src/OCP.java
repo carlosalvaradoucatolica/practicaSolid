cc5904d2fd3b85765c32a6eea94335fdad143ada
Carlos Alvarado Mancera <cralvarado92@ucatolica.edu.co>
2023-02-22 13:31:24 -0500

    OCP implementation finished

--- src/OCP.java
+++ src/OCP.java
@@ -1,58 +1,40 @@
-//Se define clase abstracta para hacer herenecia posteriormente y para definir metodo abstracto
+
 public abstract class Figura {
-    //Se elimina public por defecto
-  abstract void dibujar();
+    public void dibujar() {
+    };
 }
 
-public class Cuadrado extends Figura {
-    @Override
-    void dibujar() {
-        //Se implemente metodo para dibujar
-        System.out.println("Dibujando Cuadrado");
-    }
+public abstract class Cuadrado extends Figura {
 }
 
-public class Circulo extends Figura {
-    @Override
-    void dibujar() {
-        //Se implemente metodo para dibujar
-        System.out.println("Dibujando Circulo");
-    }
+public abstract class Circulo extends Figura {
+}
+
+public abstract class Rombo extends Figura {
 }
 
 public class Figuras {
+    Vector<Cuadrado> cuadrados = new Vector<Cuadrado>();
+    Vector<Circulo> circulos = new Vector<Circulo>();
 
-  Vector<Cuadrado> cuadrados = new Vector<Cuadrado>();
-  Vector<Circulo> circulos = new Vector<Circulo>();
-
-  public void addCirculo(Circulo c) {
-    circulos.add(c);
-  }
-
-  public void addCuadrado(Cuadrado c) {
-    cuadrados.add(c);
-  }
-
-  public void dibujarFiguras() {
-    Enumeration<Cuadrado> cuads = cuadrados.elements();
-    Cuadrado c;
-    while (cuads.hasMoreElements()) {
-      c = cuads.nextElement();
-      c.dibujar();
+    public void addFigura(Figura2 f) {
+        figuras.add(f);
     }
-    Enumeration<Circulo> circs = circulos.elements();
-    Circulo ci;
-    while (cuads.hasMoreElements()) {
-      ci = circs.nextElement();
-      ci.dibujar();
+
+    public void dibujarFiguras() {
+        for (Figura2 f : figuras) {
+            f.dibujar();
+        }
     }
-  }
 }
+
 // Consultas:
 // 1. ¿Cumple la clase Figuras el Principio OCP. Justifica tu respuesta.
-    // Respuesta: No, ya que si bien está abierta para la extensión, no está cerrada para la modificación, pues se puede
-    // modificar el metodo "dibujar" desde la implementación y no desde las clases particulares, inclusive siendo iguales.
-// 2. En caso de que no lo cumpla, modifica las clase para cumpla este criterio. Modificado
+    //Respuesta: NO, ya que no está cerrada para la modificación y abierta para la extensión.
+    // Si se desea agregar una nueva figura a la clase figura, el dia de mañana que se implemente un rombo por ejemplo,
+    // se tendría que editar dicha clase "Figuras", por tanto no cumple con el principio OCP.
+// 2. En caso de que no lo cumpla, modifica las clase para cumpla este criterio.
+    // Codigo se modifica, agregando metodo generico addFigura y dibujarFigura, que recibe cualquier tipo de Figura
 // 3. ¿Consideras que la tarea realizada es una refactorización? Justifica tu respuesta. 
     // Respuesta: Si, pues se realizan cambios a fin de cumplir con el principio OCP
     // sin cambiar en absoluto la funcionalidad del codigo.
